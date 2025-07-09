1024c87175226b65ec0128d574f47722bfcbac43
nehalgajraj <nehal.gajraj.52@gmail.com>
2025-07-09 17:23:00 +0530

    .

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -8,9 +8,6 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
 
   return (
     <article class="popover-hint">
-      <h1>404</h1>
-      <p>{i18n(cfg.locale).pages.error.notFound}</p>
-      <a href={baseDir}>{i18n(cfg.locale).pages.error.home}</a>
     </article>
   )
 }
